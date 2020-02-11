63330223c7960ba1187f704815177a9ec4c71f51
Michael Naumov <mihail.naumov23@gmail.com>
2020-02-11 11:03:34 +0200

    initial commit

---
+++ src/build/precache-manifest.7988a6644904e68968c885e04c7fe2dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e6659f78f22fd14b247f25ba0cd9829",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3482c6c06b9ee93cf580",
+    "url": "/static/css/main.84b4ca96.chunk.css"
+  },
+  {
+    "revision": "3cb37e72c89f550b30a5",
+    "url": "/static/js/2.d508b2ea.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.d508b2ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3482c6c06b9ee93cf580",
+    "url": "/static/js/main.9bea5c0a.chunk.js"
+  },
+  {
+    "revision": "1a6842cff40cdbd7ee88",
+    "url": "/static/js/runtime-main.d7f357ff.js"
+  }
+]);
